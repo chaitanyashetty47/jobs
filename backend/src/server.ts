c53c2e7678de03d7f2927f5c714962f7e9dcc179
Chaitanya Shetty <chaitanyashetty47@gmail.com>
2024-11-02 23:50:31 +0530

    small change

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,5 +11,5 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port:  ${PORT}`);
 });
